b078e3f4a334e995d601ff08a4c4cb57ef6a61f1
Jack Nehme <jack.nehme@gmail.com>
2017-11-06 19:37:38 -0800

    added jade templating to project

--- library/app.js
+++ library/app.js
@@ -2,19 +2,20 @@ var express = require('express');
 
 var app = express();
 
-var port = 5000;
+var port = process.env.PORT || 5000;
 
 app.use(express.static('public'));
-app.use(express.static('src/views'));
+app.set('views', './src/views');
+app.set('view engine', 'jade');
 
 app.get('/', function(req, res) {
-    res.send('Hello World');
+    res.render('index', { list: ['a', 'b', 'c'] });
 });
 
 app.get('/books', function(req, res) {
     res.send('Hello Books');
 });
 
-app.listen(5000, function(err) {
+app.listen(port, function(err) {
     console.log('running server on port ' + port);
 });
--- library/gulpfile.js
+++ library/gulpfile.js
@@ -1,9 +1,9 @@
 var gulp = require('gulp');
 var jshint = require('gulp-jshint');
 var jscs = require('gulp-jscs');
+var nodemon = require('gulp-nodemon');
 
 var jsFiles = ['*.js', 'src/**/*.js'];
-
 
 gulp.task('style', function() {
     return gulp.src(jsFiles)
@@ -30,8 +30,23 @@ gulp.task('inject', function() {
         ignorePath: '../../public'
     };
 
-    return gulp.src('./src/views/*.html')
+    return gulp.src('./src/views/*.jade')
         .pipe(wiredep(options))
         .pipe(inject(injectSrc, injectOptions))
         .pipe(gulp.dest('./src/views'));
+});
+
+gulp.task('serve', ['style', 'inject'], function() {
+    var options = {
+        script: 'app.js',
+        delayTime: 1,
+        env: {
+            'PORT': 3000
+        },
+        watch: jsFiles
+    };
+
+    return nodemon(options).on('restart', function(ev) {
+        console.log('Restarting....');
+    });
 });
