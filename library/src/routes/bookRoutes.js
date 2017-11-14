dcf94c94b80dd532f5256ae439550dbecf8fc451
Jack Nehme <jack.nehme@gmail.com>
2017-11-13 22:28:35 -0800

    Bad implementation of mssql

--- library/src/routes/bookRoutes.js
+++ library/src/routes/bookRoutes.js
@@ -1,6 +1,7 @@
 var express = require('express');
-
 var bookRouter = express.Router();
+var sql = require('mssql');
+
 var router = function(nav) {
     var books = [{
         title: 'War and Peace',
@@ -39,21 +40,56 @@ var router = function(nav) {
         read: false
     }];
     bookRouter.route('/').get(function(req, res) {
-        res.render('bookListView', {
-            title: 'Books',
-            nav: nav,
-            books: books
+        var request = new sql.Request();
+        request.query('select * from books', function(err, recordset) {
+            console.log(err);
+            res.render('bookListView', {
+                title: 'Books',
+                nav: nav,
+                books: books
+            });
         });
+        // res.render('bookListView', {
+        //     title: 'Books',
+        //     nav: nav,
+        //     books: books
+        // });
     });
 
-    bookRouter.route('/:id').get(function(req, res) {
-        var id = req.params.id;
-        res.render('bookView', {
-            title: 'Books',
-            nav: nav,
-            book: books[id]
+    bookRouter.route('/:id')
+        .all(function(req, res, next) {
+            var id = req.params.id;
+            var ps = new sql.PreparedStatement();
+            ps.input('id', sql.Int)
+            ps.prepare('select * from books where id = @id', function(err) {
+                    ps.execute({ id: req.params.id }, function(err, recordset) {
+                        if (recordset.length === 0) {
+                            res.status(404).send('Not Found');
+                        } else {
+                            req.book = recordset[0];
+                            next();
+                        }
+                    });
+                    console.log(err);
+                })
+                // res.render('bookView', {
+                //     title: 'Books',
+                //     nav: nav,
+                //     book: books[id]
+                // });
+        })
+        .get(function(req, res) {
+            res.render('bookView', {
+                title: 'Books',
+                nav: nav,
+                book: req.book
+            });
+            // res.render('bookView', {
+            //     title: 'Books',
+            //     nav: nav,
+            //     book: books[id]
+            // });
         });
-    });
     return bookRouter;
 };
 
