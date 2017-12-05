0fa14cad819182d7e9943379b2829a50504fe2ac
Jack Nehme <jack.nehme@gmail.com>
2017-12-04 22:05:38 -0800

    Added User Sign In

--- library/src/config/strategies/local.strategy.js
+++ library/src/config/strategies/local.strategy.js
@@ -1,11 +1,28 @@
 var passport = require('passport');
 var LocalStrategy = require('passport-local').Strategy;
+var mongodb = require('mongodb').MongoClient;
 
 module.exports = function() {
     passport.use(new LocalStrategy({
         usernameField: 'userName',
         passwordField: 'password'
     }, function(username, password, done) {
+        var url = 'mongodb://localhost:27017/libraryApp';
+        mongodb.connect(url, function(err, db) {
+            var collection = db.collection('user');
+            collection.findOne({
+                    username: username
+                },
+                function(err, results) {
+                    if (results.password === password) {
+                        var user = results;
+                        done(null, user);
+                    } else {
+                        done(null, false, { message: 'Bad password' });
+                    }
+                }
+            );
+        });
         var user = {
             username: username,
             password: password
